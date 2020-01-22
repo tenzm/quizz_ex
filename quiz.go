7c428f77c0097b6cc92771e1646cc43db3b95c7c
Maksim Blinov <tenzm@mail.ru>
2020-01-22 16:42:56 +0300

    solved task 1 & 2

--- quiz.go
+++ quiz.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strconv"
 )
 
 type QuestionReader interface {
@@ -28,15 +29,27 @@ func readCsv(filename string) []Question {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(records[0])
+	question := make([]Question, len(records))
+	for i := range records {
+		question[i].question = records[i][0]
+		question[i].answer = records[i][1]
+	}
 	// Допишите код здесь
-	return nil
+	return question
 }
 
 func main() {
 	total := 0
-
-	questions := []Question{{"1 + 1", "2"}, {"2 + 2", "4"}}
+	questions := readCsv("problems.csv")
+	for i, _ := range questions {
+		fmt.Println(questions[i].question)
+		var answ string
+		fmt.Scan(&answ)
+		if answ == questions[i].answer {
+			total += 1
+		}
+	}
+	fmt.Println("You got " + strconv.Itoa(total) + "/" + strconv.Itoa(len(questions)))
 	// Пройтись циклом. Вывести вопрос, предложить пользователю ввести ответ.
 	// Если ответ правильный, увеличить total.
 	// for
